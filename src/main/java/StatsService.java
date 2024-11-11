721f407a9f5a3d54979f4d613125ca4394569837
Relict <vostryakovkirill@gmail.com>
2024-11-11 12:17:10 +0400

    Initial commit

---
+++ src/main/java/StatsService.java
@@ -0,0 +1,58 @@
+package ru.netology.stats;
+
+public class StatsService {
+
+    public long sumSales(long[] sales) {
+        long sum = 0;
+        for (long sale : sales) {
+            sum += sale;
+        }
+        return sum;
+    }
+
+    public double averageSales(long[] sales) {
+        return (double) sumSales(sales) / sales.length;
+    }
+
+    public int maxSales(long[] sales) {
+        int maxMonth = 0;
+        for (int i = 1; i < sales.length; i++) {
+            if (sales[i] > sales[maxMonth]) {
+                maxMonth = i;
+            }
+        }
+        return maxMonth + 1;
+    }
+
+    public int minSales(long[] sales) {
+        int minMonth = 0;
+        for (int i = 1; i < sales.length; i++) {
+            if (sales[i] < sales[minMonth]) {
+                minMonth = i;
+            }
+        }
+        return minMonth + 1;
+    }
+
+    public int countMonthsBelowAverage(long[] sales) {
+        double average = averageSales(sales);
+        int count = 0;
+        for (long sale : sales) {
+            if (sale < average) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int countMonthsAboveAverage(long[] sales) {
+        double average = averageSales(sales);
+        int count = 0;
+        for (long sale : sales) {
+            if (sale > average) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
---
+++ src/test/java/StatsServiceTest.java
@@ -0,0 +1,50 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StatsServiceTest {
+
+    @Test
+    public void testSumSales() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        assertEquals(180, service.sumSales(sales));
+    }
+
+    @Test
+    public void testAverageSales() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        assertEquals(15.0, service.averageSales(sales));
+    }
+
+    @Test
+    public void testMaxSales() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        assertEquals(6, service.maxSales(sales));
+    }
+
+    @Test
+    public void testMinSales() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        assertEquals(9, service.minSales(sales));
+    }
+
+    @Test
+    public void testCountMonthsBelowAverage() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        assertEquals(5, service.countMonthsBelowAverage(sales));
+    }
+
+    @Test
+    public void testCountMonthsAboveAverage() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        assertEquals(5, service.countMonthsAboveAverage(sales));
+    }
+}
